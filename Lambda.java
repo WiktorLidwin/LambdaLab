36b41fbf68a6dd4cdb383cfee0f3a2fbaadaedee
Wiktor <wiktorlidwin@gmail.com>
2020-01-28 09:49:30 -0500

    comments stuff

--- Lambda.java
+++ Lambda.java
@@ -16,23 +16,49 @@ public class Lambda {
             name = reader.readLine();
             char[] namearray = name.toCharArray();
             int commentsplit = namearray.length;
-            for (int i = namearray.length-1; i >=0; i--) {
-                if(namearray[i] == (';')){
+            for (int i = namearray.length - 1; i >= 0; i--) {
+                if (namearray[i] == (';')) {
                     commentsplit = i;
                 }
             }
+            boolean charfound = false;
+            int charpos = commentsplit;
+            if (namearray[commentsplit - 1] == ' ') { // for removing spaces before the comment a____;comment removes the _____(<-spaces) 
+                for (int i = commentsplit - 1; i >= 0; i--) {
+                    if (charfound == false && namearray[i] == ' ') {
+                        charpos = i;
+                    }
+                    if(namearray[i] != ' '){
+                        charfound = true;
+                    }
+                }
+            }   
+            char[] temp = new char[charpos];
+            System.arraycopy(namearray, 0, temp, 0, charpos);
 
-            char[] temp= new char[commentsplit];
-            System.arraycopy(namearray, 0, temp, 0, commentsplit);
-            System.out.println("commentsplit: "+commentsplit+" array:"+Arrays.toString(temp) );
             namearray = temp;
-            StringBuilder sbf = new StringBuilder(""); 
+            System.out.println(Arrays.toString(temp));
+            StringBuilder sbf = new StringBuilder("");
             sbf.append(namearray);
-            name = sbf.toString(); 
-            
-   
+            name = sbf.toString();
+
+            int numberofvariables = 0;
+            if(temp.length == 0 ){
+                System.out.println("bruh");
+            }
+            else if(temp[0] != ' '){
+                numberofvariables++;
+            }
+            for (int i = 0; i < temp.length-1; i++) {
+                if(temp[i] == ' ' && temp[i+1]!= ' '){
+                    numberofvariables++;
+                }
+            }
+            System.out.println(numberofvariables);
+            Variables var = new Variables(name);
+
             if (!name.equals("exit")) {
-                System.out.println(name);
+                System.out.println(var.toString());
             } else {
                 System.out.println("Goodbye!");
             }
---
+++ Variables.java
@@ -0,0 +1,11 @@
+public class Variables{
+    String name = "";
+
+    public Variables(String name){
+        this.name = name;
+    }
+
+    public String toString(){
+        return name; 
+    }
+}
